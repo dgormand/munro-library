ffd9d7db401e71fd04a7a8675196cfe16098030d
Dagna <witchi.chan@gmail.com>
2021-05-23 10:59:21 +0100

    Initial project commit

---
+++ src/test/java/com/munro/library/MunroApplicationTests.java
@@ -0,0 +1,14 @@
+package com.munro.library;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunroApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
