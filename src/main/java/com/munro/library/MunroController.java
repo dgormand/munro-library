bab460ad5c41cfb46be310036836fe08faebca64
Dagna <witchi.chan@gmail.com>
2021-05-24 01:35:01 +0100

    Adding response as json, needs tidy up and excpetion handling

---
+++ src/main/java/com/munro/library/MunroController.java
@@ -0,0 +1,29 @@
+package com.munro.library;
+
+import com.google.gson.Gson;
+import com.munro.library.entity.MunroEnum;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+
+@RestController
+public class MunroController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> search(@RequestParam LinkedHashMap<String, String> allParams) {
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        Gson gson = new Gson();
+        MunroService munroService = new MunroService();
+
+        return new ResponseEntity<>(gson.toJson(munroService.getResult(MunroEnum.INSTANCE.getMunroList(), allParams)),
+                httpHeaders, HttpStatus.OK);
+    }
+}
--- src/main/java/com/munro/library/entity/Munro.java
+++ src/main/java/com/munro/library/entity/Munro.java
@@ -1,6 +1,7 @@
 package com.munro.library.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.Expose;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,90 +13,90 @@ import lombok.Setter;
 @Setter
 public class Munro {
     @JsonProperty("Running No")
-    private Integer id;
+    private transient Integer id;
 
     @JsonProperty("DoBIH Number")
-    private String dobIhNumber;
+    private transient String dobIhNumber;
 
     @JsonProperty("Streetmap")
-    private String streetMap;
+    private transient String streetMap;
 
     @JsonProperty("Geograph")
-    private String geolink;
+    private transient String geolink;
 
     @JsonProperty("Hill-bagging")
-    private String hillBagging;
+    private transient String hillBagging;
 
     @JsonProperty("Name")
     private String name;
 
     @JsonProperty("SMC Section")
-    private String smcSection;
+    private transient String smcSection;
 
     @JsonProperty("RHB Section")
-    private String rhbSection;
+    private transient String rhbSection;
 
     @JsonProperty("_Section")
-    private String section;
+    private transient String section;
 
     @JsonProperty("Height (m)")
     private Double heightMeters;
 
     @JsonProperty("Height (ft)")
-    private Double heightFeet;
+    private transient Double heightFeet;
 
     @JsonProperty("Map 1:50")
-    private String mapScale50;
+    private transient String mapScale50;
 
     @JsonProperty("Map 1:25")
-    private String mapScale25;
+    private transient String mapScale25;
 
     @JsonProperty("Grid Ref")
     private String gridRef;
 
     @JsonProperty("GridRefXY")
-    private String gridRefXY;
+    private transient String gridRefXY;
 
     @JsonProperty("xcoord")
-    private String xCoord;
+    private transient String xCoord;
 
     @JsonProperty("ycoord")
-    private String yCoord;
+    private transient String yCoord;
 
     @JsonProperty("1891")
-    private String class1891;
+    private transient String class1891;
 
     @JsonProperty("1921")
-    private String class1921;
+    private transient String class1921;
 
     @JsonProperty("1933")
-    private String class1933;
+    private transient String class1933;
 
     @JsonProperty("1953")
-    private String class1953;
+    private transient String class1953;
 
     @JsonProperty("1969")
-    private String class1969;
+    private transient String class1969;
 
     @JsonProperty("1974")
-    private String class1974;
+    private transient String class1974;
 
     @JsonProperty("1981")
-    private String class1981;
+    private transient String class1981;
 
     @JsonProperty("1984")
-    private String class1984;
+    private transient String class1984;
 
     @JsonProperty("1990")
-    private String class1990;
+    private transient String class1990;
 
     @JsonProperty("1997")
-    private String class1997;
+    private transient String class1997;
 
     @JsonProperty("Post 1997")
     private String classPost1997;
 
     @JsonProperty("Comments")
-    private String comments;
+    private transient String comments;
 
 }
